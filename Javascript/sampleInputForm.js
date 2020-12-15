9139be34dbb47cbd0a232930c5d14792ad26c840
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-15 21:39:18 +0530

    [Prajakta]: Add Regex validation for Name

--- Javascript/sampleInputForm.js
+++ Javascript/sampleInputForm.js
@@ -5,3 +5,18 @@ salary.addEventListener('input', function () {
     output.textContent = salary.value;
 });
 
+//uc1 first name starts with capital letter and min 3 letter
+
+const text = document.querySelector('#text'); //storing the value using id=text
+const textError = document.querySelector('.text-error'); //similarly getting output for texterror 
+text.addEventListener('input', function () {
+    let nameRegex = RegExp('^[A-Z]{1}[A-Za-z]{2,}$');
+    if (nameRegex.test(text.value)) 
+    {
+        textError.textContent = "";
+    }
+    else 
+    {
+        textError.textContent = "Name is Incorrect";
+    }
+});
